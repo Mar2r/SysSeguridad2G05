10b60c0288ffacd99b30ffa2f9e7ab7c15cdcf23
Mar2r <ramosmargarita2m@gmail.com>
2026-03-18 08:10:09 -0600

    Se agregó clase de rol y DbContexto

---
+++ SysSeguridad2G05.DAL/DBContexto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;   
+using SysSeguridad2G05.EN;
+
+namespace SysSeguridad2G05.DAL
+{
+    public class DBContexto : DbContext
+    {
+        public DbSet<Rol> Rol { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DBSysSeguridad;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30");
+        }
+    }
+}
---
+++ SysSeguridad2G05.DAL/RolDAL.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+using SysSeguridad2G05.EN;
+
+namespace SysSeguridad2G05.DAL
+{
+    public class RolDAL
+    {
+        //metodo para crear un nuevo rol
+        public static async Task<int> CrearAsync(Rol rol)
+        {
+            int result = 0;
+            using (var dbContexto = new DBContexto())
+            {
+                dbContexto.Rol.Add(rol);
+                result = await dbContexto.SaveChangesAsync();
+            }
+            return result;
+        }
+
+        //metodo para modificar un rol
+        public static async Task<int> ModificarAsync(Rol pRol)
+        {
+            int result = 0;
+
+            using (var dbContexto = new DBContexto())
+            {
+                var rol = await dbContexto.Rol.FirstOrDefaultAsync(s => s.IdRol == pRol.IdRol);
+                rol.Nombre = pRol.Nombre;
+                dbContexto.Update(rol);
+                result = await dbContexto.SaveChangesAsync();
+            }
+            return result;
+        }
+
+        //Metodo para eliminar un rol
+        public static async Task<int> EliminarAsync( Rol pRol)
+        {
+            int result = 0;
+            using (var dbContexto = new DBContexto())
+            {
+                var rol = await dbContexto.Rol.FirstOrDefaultAsync(s => s.IdRol == pRol.IdRol);
+                dbContexto.Rol.Remove(rol);
+                result = await dbContexto.SaveChangesAsync();
+            }
+            return result;
+        }
+    }
+}
